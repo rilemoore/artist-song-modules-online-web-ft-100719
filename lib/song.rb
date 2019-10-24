23b25d40b24c8cb77e4e8ea98f6b8a326ea208a0
Riley Moore <rilemoore@gmail.com>
2019-10-24 00:48:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,9 +11,9 @@ class Song
 
   
 
-  def initialize
-    self.class.all << self
-  end
+  # def initialize
+  #   self.class.all << self
+  # end
 
   # def self.find_by_name(name)
   #   @@all.detect{|a| a.name == name}
