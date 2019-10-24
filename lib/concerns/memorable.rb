e9abbaf1431976ce531b3a28f7d3257b46fabe12
Riley Moore <rilemoore@gmail.com>
2019-10-24 00:46:31 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,19 @@
 
 
 module Memorable
-  def reset_all
-    all.clear
-  end
+  module ClassMethods
+    def reset_all
+     all.clear
+    end
 
-  def count
-    all.count
+   def count
+     all.count
+   end
+  end
+  
+  module InstanceMethods
+    def initialize
+      
+    end
   end
 end
